4aa381c6edc0f6d792178f59e1d73e5019b6c3cc
LESANF <nagong1000@naver.com>
2021-12-03 20:10:11 +0900

    docs: End 2884

---
+++ Baekjoon/if문/2884/2884.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const input = fs.readFileSync('예제.txt').toString().trim().split(' ');
+
+let a = 0;
+let b = 0;
+
+a = parseInt(input[0]);
+b = parseInt(input[1]);
+
+b -= 45;
+
+if (b < 0) {
+    a -= 1;
+    if (a < 0) {
+        a = 23;
+    }
+
+    b *= -1;
+}
+
+if (b > 59) {
+    a += 1;
+    if (a > 23) {
+        a = 0;
+    }
+    b -= 60;
+}
+
+console.log(a + ' ' + b);
