4b3c96c24e7687cf024ab981ef1d6d7366114527
LESANF <nagong1000@naver.com>
2021-12-06 00:55:13 +0900

    docs: End 11022

---
+++ Baekjoon/for문/11022/11022.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const input = fs.readFileSync('예제.txt').toString().trim().split('\n');
+
+let a = parseInt(input[0]);
+let ans = '';
+
+for (let i = 1; i <= a; i++) {
+    ans += `Case #${i}: ${parseInt(input[i].split(' ')[0])} + ${parseInt(input[i].split(' ')[1])} = ${
+        parseInt(input[i].split(' ')[0]) + parseInt(input[i].split(' ')[1])
+    }\n`;
+}
+
+console.log(ans);
