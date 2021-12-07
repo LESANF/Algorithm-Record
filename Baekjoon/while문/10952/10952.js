601860d7ddc65a7ea53a7227c2b2319b894589f8
LESANF <nagong1000@naver.com>
2021-12-07 23:00:19 +0900

    docs: End 10952

---
+++ Baekjoon/while문/10952/10952.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('예제.txt').toString().trim().split('\n');
+let cnt = 0;
+while (parseInt(input[cnt].split(' ')[0]) !== 0 && parseInt(input[cnt].split(' ')[1]) !== 0) {
+    console.log(parseInt(input[cnt].split(' ')[0]) + parseInt(input[cnt].split(' ')[1]));
+    cnt++;
+}
