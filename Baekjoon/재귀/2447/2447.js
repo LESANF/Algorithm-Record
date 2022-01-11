5074f47839132f8cb6f65a55de4180488ff0c343
LESANF <nagong1000@naver.com>
2022-01-11 14:46:56 +0900

    add: 기본수학2

--- Baekjoon/재귀/2447/2447.js
+++ Baekjoon/재귀/2447/2447.js
@@ -1,3 +1,1 @@
 let num = +require('fs').readFileSync('예제.txt').toString().trim();
-const fibonacchi = (num) => (num <= 1 ? num : fibonacchi(num - 1) + fibonacchi(num - 2));
-console.log(fibonacchi(num));
