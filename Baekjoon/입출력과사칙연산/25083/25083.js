ded6b6101541af86c53902bd6d33776edd49f232
LESANF <nagong1000@naver.com>
2022-05-03 23:51:27 +0900

    docs: Ing 25083

---
+++ Baekjoon/입출력과사칙연산/25083/25083.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('예제.txt').toString().trim().split(' ');
+
+//          ,r'"7
+// r`-_   ,'  ,/
+//  \. ". L_r'
+//    `~\/
+//       |
+//       |
