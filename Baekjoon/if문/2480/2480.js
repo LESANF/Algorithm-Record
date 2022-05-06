4ea260f75aa41b63f738e3a4cbf76008dd32fd3d
LESANF <nagong1000@naver.com>
2022-05-06 23:44:05 +0900

    docs: Ing 2480

--- Baekjoon/if문/2480/2480.js
+++ Baekjoon/if문/2480/2480.js
@@ -9,26 +9,5 @@ const input = fs
 const diceNum1 = input[0];
 const diceNum2 = input[1];
 const diceNum3 = input[2];
-let cnt = 0;
 
-const test = input.filter((v) => console.log(v + 1));
-
-// for (let i = 0; i < input.length; i++) {
-//     if (diceNum1 === input[i + 1]) {
-//         cnt++;
-//     }
-// }
-
-// switch (cnt) {
-//     case 0:
-//         console.log(Math.max(...input) * 100);
-//         break;
-//     case 1:
-//         console.log('two same');
-//         break;
-//     case 2:
-//         console.log(10000 + input[0] * 1000);
-//         break;
-// }
-
-//console.log(cnt);
+if (diceNum1 === diceNum2 && diceNum2 === diceNum3) console.log(10000 + diceNum1 * 1000);
