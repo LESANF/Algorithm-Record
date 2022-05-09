7e582eeb01d4f9c1adeab711becbba7c7a3b2bfe
LESANF <nagong1000@naver.com>
2022-05-10 06:00:06 +0900

    docs: Done 11653

--- Baekjoon/기본수학2/11653/11653.js
+++ Baekjoon/기본수학2/11653/11653.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const input = +fs.readFileSync('예제.txt').toString().trim();
 
 let num = input;
-let quotient = 0;
 let divide = 2;
 
-while (num % divide === 0) {
-    if (Number.isInteger(num / divide)) {
-        num /= divide;
-    } else divide++;
-
-    console.log(divide, num);
+while (num !== 1) {
+    if (Math.floor(num % divide) !== 0) {
+        divide++;
+    } else {
+        num = Math.floor(num / divide);
+        console.log(divide);
+    }
 }
