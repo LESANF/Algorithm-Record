0a66ba636fe89a94744c2fc2e3667fba528c02e2
LESANF <nagong1000@naver.com>
2022-05-11 03:38:10 +0900

    docs: Done 1978

--- Baekjoon/기본수학2/1978/1978.js
+++ Baekjoon/기본수학2/1978/1978.js
@@ -1,2 +1,16 @@
 const fs = require('fs');
-const input = fs.readFileSync('예제.txt').toString().trim();
+const input = fs.readFileSync('예제.txt').toString().trim().split('\n');
+
+// 소수는 1과 자기자신이외에 나눠지는값이 있으면안됨.
+let cnt = 0;
+let ans = 0;
+for (let i = 0; i < input[0]; i++) {
+    for (let j = 1; j <= input[1].split(' ')[i]; j++) {
+        if (+input[1].split(' ')[i] === 1) continue;
+        if (input[1].split(' ')[i] % j === 0) cnt++;
+    }
+    if (cnt === 2) ans++;
+    cnt = 0;
+}
+
+console.log(ans);
