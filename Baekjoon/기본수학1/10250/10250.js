845c339579ed565fbe1250c33e35d5715f5a119c
LESANF <nagong1000@naver.com>
2022-05-24 23:18:48 +0900

    docs: Ing 10250

--- Baekjoon/기본수학1/10250/10250.js
+++ Baekjoon/기본수학1/10250/10250.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
-const [a, b, v] = fs.readFileSync('예제.txt').toString().trim().split(' ').map(Number);
-console.log(Math.ceil((v - b) / (a - b)));
+const input = fs.readFileSync('예제.txt').toString().trim().split('\n');
+
+const testCase = input.shift();
+
+for (let i = 0; i < testCase; i++) {
+    const height = +input[i].split(' ')[0];
+    const width = +input[i].split(' ')[1];
+    const guest = +input[i].split(' ')[2];
+
+    const roomState = Array.from(Array(height), () => Array(width).fill(false));
+    console.log(roomState);
+}
