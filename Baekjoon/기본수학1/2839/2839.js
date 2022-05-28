e9a9f5650c9e1a8a9c328f542866537e3ddc575c
LESANF <nagong1000@naver.com>
2022-05-29 05:44:39 +0900

    docs: End 2839

--- Baekjoon/기본수학1/2839/2839.js
+++ Baekjoon/기본수학1/2839/2839.js
@@ -1,27 +1,20 @@
 const fs = require('fs');
-const input = fs.readFileSync('예제.txt').toString().trim();
+let input = +fs.readFileSync('예제.txt').toString().trim();
 
-let layer = 1;
-let index = 0;
-let numerator = 0;
-let denominator = 0;
-let result = '';
+let five,
+    three = 0;
+while (1) {
+    if (input % 5 === 0) {
+        five = input / 5;
+        console.log(five + three);
+        break;
+    }
 
-while (Math.floor((layer * (layer + 1)) / 2) < input) layer++;
-index = Math.abs(Math.floor(((layer - 1) * layer) / 2) - input);
+    if (input < 0) {
+        console.log(-1);
+        break;
+    }
 
-if (layer % 2 === 0) {
-    numerator = index;
-
-    if (index === 1) denominator = layer;
-    else denominator = layer - index + 1;
-
-    console.log(`${numerator}/${denominator}`);
-} else {
-    denominator = index;
-
-    if (index === 1) numerator = layer;
-    else numerator = layer - index + 1;
-
-    console.log(`${numerator}/${denominator}`);
+    input -= 3;
+    three++;
 }
