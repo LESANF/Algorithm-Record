fc0ae9deb19b4ba0bf33dab89388288913056337
LESANF <nagong1000@naver.com>
2022-06-12 21:59:26 +0900

    docs: Ing 2447

--- Baekjoon/재귀/2447/2447.js
+++ Baekjoon/재귀/2447/2447.js
@@ -1,1 +1,9 @@
 let num = +require('fs').readFileSync('예제.txt').toString().trim();
+
+const pattern = (num) => {
+    const center = num / 2;
+    console.log(center);
+    for (let i = 0; i < num; i++) {}
+};
+
+pattern(num);
