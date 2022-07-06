1bf1d8209cd634d8ba24412e25b37850ebabd1c4
LESANF <nagong1000@naver.com>
2022-07-07 07:52:12 +0900

    docs: End Inflean Section1 Q16

---
+++ Inflean/JS알고리즘강의/기본문제/중복문자제거/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('예제.txt').toString().trim();
+
+function solution(input) {
+    let ans;
+    const prevArr = input.split('');
+    const newArr = prevArr.filter((v, i) => prevArr.indexOf(v) === i);
+    ans = newArr.join('');
+    return ans;
+}
+
+console.log(solution(input));
