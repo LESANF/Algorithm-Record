b4efea9caa7de38025e9c98b6d1ac4ca427e6b15
LESANF <nagong1000@naver.com>
2022-07-28 23:40:10 +0900

    docs: End Inflean Section2 Q7

--- Inflean/JS알고리즘강의/1,2차원배열탐색/봉우리/index.js
+++ Inflean/JS알고리즘강의/1,2차원배열탐색/봉우리/index.js
@@ -10,6 +10,23 @@ for (let i = 1; i <= n; i++) {
 
 function solution(square) {
     let ans = 0;
+    //상하좌우 (-1,0), (1, 0), (0, -1), (0, 1)
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+
+    for (let i = 0; i < square.length; i++) {
+        for (let j = 0; j < square.length; j++) {
+            let flag = false;
+            for (let k = 0; k < 4; k++) {
+                let dx1 = i + dx[k];
+                let dy1 = j + dy[k];
+                console.log(dx1, dy1);
+                // if (square[dx1][dy1] > square[i][j]) break;
+                // else flag = true;
+            }
+            if (flag) ans++;
+        }
+    }
 
     return ans;
 }
