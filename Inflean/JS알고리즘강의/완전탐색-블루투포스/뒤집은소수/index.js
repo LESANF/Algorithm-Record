6cf178d709b2b98d0e3aa6de50a88441defd5f91
LESANF <nagong1000@naver.com>
2022-09-26 20:37:07 +0900

    docs: End 2798

---
+++ Inflean/JS알고리즘강의/완전탐색-블루투포스/뒤집은소수/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('예제.txt').toString().trim().split('\n');
+
+function solution(input) {
+    let ans = 0;
+    console.log(input);
+    return ans;
+}
+
+console.log(solution(input));
