4d8ca1f515b06b77a47676a20dedd0ba8e8a9817
LESANF <nagong1000@naver.com>
2023-05-21 09:29:22 +0900

    fix: folder name

--- Baekjoon/기본수학1/1712/1712.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const [a, b, c] = fs.readFileSync('예제.txt').toString().trim().split(' ').map(Number);
-c - b <= 0 ? console.log(-1) : Math.floor(a / (c - b) + 1);
--- Baekjoon/기본수학1/2292/2292.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync('예제.txt').toString().trim();
-let sum = 1;
-let n = 0;
-while (sum + 6 * n < +input) {
-    sum += 6 * n;
-    n++;
-}
-console.log(n + 1);
