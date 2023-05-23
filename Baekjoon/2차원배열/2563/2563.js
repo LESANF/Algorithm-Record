7acd9e258da5a5a36d2c3e087616a3fb443afda2
LESANF <nagong1000@naver.com>
2023-05-23 21:45:56 +0900

    fix: folder structure

--- Baekjoon/2차원배열/2563/2563.js
+++ Baekjoon/2차원배열/2563/2563.js
@@ -1,6 +1,26 @@
 const fs = require("fs");
-const [n, b] = fs.readFileSync("예제.txt").toString().trim().split(" ");
-//test
-for (v of n) {
-  console.log(v);
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let cnt = 0;
+let array2d = Array.from({ length: 100 }, (_) => Array.from({ length: 100 }, (_) => false));
+
+const coloredPaperEa = +input.shift();
+
+for (let i = 0; i < coloredPaperEa; i++) {
+  const xPos = +input[i].split(" ")[0] - 1;
+  const yPos = +input[i].split(" ")[1] - 1;
+
+  for (let j = xPos; j < xPos + 10; j++) {
+    for (let z = yPos; z < yPos + 10; z++) {
+      array2d[j][z] = true;
+    }
+  }
 }
+
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    if (array2d[i][j] === true) cnt++;
+  }
+}
+
+console.log(cnt);
