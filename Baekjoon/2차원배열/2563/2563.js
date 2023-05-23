afb3975e47f603f4518d0a9fd8521cc9c18c2529
LESANF <nagong1000@naver.com>
2023-05-23 21:27:33 +0900

    End 2563

--- Baekjoon/2차원배열/2563/2563.js
+++ Baekjoon/2차원배열/2563/2563.js
@@ -1,26 +1,6 @@
 const fs = require("fs");
-const input = fs.readFileSync("예제.txt").toString().trim().split("\n");
+const [n, b] = fs.readFileSync("예제.txt").toString().trim().split(" ");
 
-let cnt = 0;
-let array2d = Array.from({ length: 100 }, (_) => Array.from({ length: 100 }, (_) => false));
-
-const coloredPaperEa = +input.shift();
-
-for (let i = 0; i < coloredPaperEa; i++) {
-  const xPos = +input[i].split(" ")[0] - 1;
-  const yPos = +input[i].split(" ")[1] - 1;
-
-  for (let j = xPos; j < xPos + 10; j++) {
-    for (let z = yPos; z < yPos + 10; z++) {
-      array2d[j][z] = true;
-    }
-  }
+for (v of n) {
+  console.log(v);
 }
-
-for (let i = 0; i < 100; i++) {
-  for (let j = 0; j < 100; j++) {
-    if (array2d[i][j] === true) cnt++;
-  }
-}
-
-console.log(cnt);
