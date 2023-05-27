225f7851aede78ae13513c6e957243974f2b8854
LESANF <nagong1000@naver.com>
2023-05-27 21:43:23 +0900

    End: 1085

--- Baekjoon/기하:직사각형과삼각형/1085/1085.js
+++ Baekjoon/기하:직사각형과삼각형/1085/1085.js
@@ -1,2 +1,19 @@
 const fs = require("fs");
-const input = fs.readFileSync("예제.txt").toString().trim().split(" ");
+const [x, y, w, h] = fs.readFileSync("예제.txt").toString().trim().split(" ").map(Number);
+
+// 상, 하, 좌, 우 를 구해서 가직 작은 값이 답
+// 현재 위치는 x, y
+// 사각형의 끝 지점은 w, h
+// 사각형의 가로길이는 w
+// 사각형의 세로길이는 h
+// h - y (상)
+// w - x (우)
+// y (하)
+// x (좌)
+
+let shortestDistance = Number.MAX_SAFE_INTEGER;
+if (shortestDistance >= h - y) shortestDistance = h - y;
+if (shortestDistance >= w - x) shortestDistance = w - x;
+if (shortestDistance >= y) shortestDistance = y;
+if (shortestDistance >= x) shortestDistance = x;
+console.log(shortestDistance);
