5332b066aa0e8f4db4d19ab7e0e29f436181a47e
LESANF <nagong1000@naver.com>
2023-07-04 02:57:54 +0900

    End 17103

--- Baekjoon/약수와배수소수2/17103/17103.js
+++ Baekjoon/약수와배수소수2/17103/17103.js
@@ -6,31 +6,25 @@ const [N, ...rest] = fs
   .split("\n")
   .map(Number);
 
-for (let x = 0; x < N; x++) {
-  const currentNumber = rest[x];
-
-  let board = Array.from({ length: currentNumber }).fill(true);
-  for (let y = 2; y <= Math.floor(Math.sqrt(currentNumber)); y++) {
-    for (let z = 2; z * y <= currentNumber; z++) {
-      board[z * y - 1] = false;
-    }
+let board = Array.from({ length: Math.max(...rest) + 1 }).fill(true);
+for (let y = 2; y <= Math.floor(Math.sqrt(Math.max(...rest))); y++) {
+  for (let z = 2; z * y <= Math.max(...rest); z++) {
+    board[z * y] = false;
   }
-
-  const primeMap = new Map();
-  board.forEach((value, index) => {
-    if (value && index !== 0) primeMap.set(index + 1, index + 1);
-  });
-
-  const setPartition = new Map();
-  for (let value of primeMap.keys()) {
-    primeMap.has(currentNumber - value) &&
-      setPartition.set(value, currentNumber - value);
-  }
-
-  for (let [key, value] of setPartition) {
-    if (key === value) continue;
-    setPartition.get(value) === key && setPartition.delete(key);
-  }
-
-  console.log(setPartition.size);
 }
+
+board[0] = false;
+board[1] = false;
+
+let result = "";
+
+rest.forEach((value) => {
+  let cnt = 0;
+  for (let i = 2; i <= value / 2; i++) {
+    if (board[i] && board[value - i]) cnt++;
+  }
+
+  result += `${cnt}\n`;
+});
+
+console.log(result);
